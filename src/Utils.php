5d872c2dd5924147ae16db392802f08eaab5b57b
vanechq <vanechq@gmail.com>
2022-10-11 17:03:17 +1000

    add tests

---
+++ src/Utils.php
@@ -0,0 +1,8 @@
+<?php
+namespace Hexlet\Phpunit\Utils;
+
+// Эта функция переворачивает переданную строку
+function reverseString($string)
+{
+    return implode(array_reverse(str_split($string)));
+}
